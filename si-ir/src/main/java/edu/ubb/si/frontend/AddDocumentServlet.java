1c1bdab39f5bba22a0b60aab76d709e30a8ddeb4
pszabolcs <pszabolcs93@gmail.com>
2015-12-23 21:56:08 +0200

    Uj dokumentumok hozzaadasahoz egy html page

---
+++ si-ir/src/main/java/edu/ubb/si/frontend/AddDocumentServlet.java
@@ -0,0 +1,48 @@
+package edu.ubb.si.frontend;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ubb.si.model.Document;
+import edu.ubb.si.solr.DocumentManager;
+
+/**
+ * Servlet implementation class AddDocumentServlet
+ */
+@WebServlet("/addoc")
+public class AddDocumentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddDocumentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = request.getParameter("url");
+		String caption = request.getParameter("caption");
+		String context = request.getParameter("context");
+		
+		Document document = new Document(url, caption, context);
+		DocumentManager manager = new DocumentManager();
+		try {
+			manager.addDocument(document);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		response.sendRedirect("index.jsp");
+	}
+
+}
--- si-ir/src/main/webapp/index.jsp
+++ si-ir/src/main/webapp/index.jsp
@@ -1,5 +1,17 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<form method="post" name="adddoc" action="addoc">
+	<label for="url">URL:</label>
+	<input type="text" name="url"/>
+	
+	<label for="caption">Caption:</label>
+	<input type="text" name="caption"/>
+	
+	<label for="context">Context:</label>
+	<input type="text" name="context"/>
+	
+	<input type="submit" name="save" value="Add document"/>
+</form>
 </body>
 </html>
