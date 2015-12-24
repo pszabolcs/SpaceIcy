896ac3cdcd57c3496d9bd97cc58592e62df8f2b8
pszabolcs <pszabolcs93@gmail.com>
2015-12-24 13:42:55 +0200

    Add simple query method to DocumentManager

--- si-ir/src/main/java/edu/ubb/si/solr/DocumentManager.java
+++ si-ir/src/main/java/edu/ubb/si/solr/DocumentManager.java
@@ -1,11 +1,16 @@
 package edu.ubb.si.solr;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 
 import edu.ubb.si.model.Document;
@@ -35,5 +40,22 @@ public class DocumentManager {
 		for (Document d: documents) {
 			addDocument(d);
 		}
+	}
+	
+	public List<String> processQuery(String query) throws SolrServerException, IOException {
+		String qStr = "image_name:" + query + " OR caption:" + query + " OR html_context:" + query;
+		
+		SolrQuery q = new SolrQuery(qStr);
+		q.setRows(100);
+		QueryResponse res = client.query(q);
+		SolrDocumentList list = res.getResults();
+		
+		List<String> resultUrls = new ArrayList<String>();
+		
+		for (SolrDocument doc : list) {
+			resultUrls.add((String) doc.getFieldValue("url"));
+		}
+		
+		return resultUrls;
 	}
 }
