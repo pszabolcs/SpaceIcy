e2e760e80572daf31c5286c43bfe95780d29c88e
csanad <scsanad@gmail.com>
2015-12-24 02:39:14 +0200

    context filtering

--- si-ir/src/main/java/edu/ubb/si/crawler/Controller.java
+++ si-ir/src/main/java/edu/ubb/si/crawler/Controller.java
@@ -1,5 +1,8 @@
 package edu.ubb.si.crawler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
@@ -10,32 +13,34 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
  * Created by csanad on 12/13/15.
  */
 public class Controller {
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     public static void main(String[] args) throws Exception {
-        String crawlStorageFolder = "/home/csanad/Documents/CrawlerDoc";
+
         int numberOfCrawlers = 7;
 
         CrawlConfig config = new CrawlConfig();
-        config.setCrawlStorageFolder(crawlStorageFolder);
+        config.setMaxDepthOfCrawling(3);
+        config.setCrawlStorageFolder("/home/csanad/Documents/CrawlerDoc");
+        config.setMaxPagesToFetch(10);
 
-        /*
-         * Instantiate the controller for this crawl.
-         */
+    /*
+     * Since images are binary content, we need to set this parameter to
+     * true to make sure they are included in the crawl.
+     */
+        String[] crawlDomains = {"http://www.wareable.com"};
+
         PageFetcher pageFetcher = new PageFetcher(config);
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
         CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+        for (String domain : crawlDomains) {
+            controller.addSeed(domain);
+        }
 
-        /*
-         * For each crawl, you need to add some seed urls. These are the first
-         * URLs that are fetched and then the crawler starts following links
-         * which are found in these pages
-         */
-        controller.addSeed("http://www.cs.ubbcluj.ro/");
+        ImageCrawler.configure(crawlDomains);
 
-        /*
-         * Start the crawl. This is a blocking operation, meaning that your code
-         * will reach the line after this only when crawling is finished.
-         */
+        System.out.println("Starting the crawler");
         controller.start(ImageCrawler.class, numberOfCrawlers);
     }
 }
--- si-ir/src/main/java/edu/ubb/si/crawler/ImageCrawler.java
+++ si-ir/src/main/java/edu/ubb/si/crawler/ImageCrawler.java
@@ -2,14 +2,18 @@ package edu.ubb.si.crawler;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
+import org.jsoup.select.NodeVisitor;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -18,75 +22,76 @@ import java.util.regex.Pattern;
 public class ImageCrawler extends WebCrawler {
 
     private static final Pattern filters = Pattern
-            .compile(".*(\\.(css|js|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|pdf"
-                    + "|bmp|gif|jpe?g|png|tiff"
-                    + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+            .compile(".*(\\.(css|js|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|pdf" + "|bmp|gif|jpe?g|png|tiff?" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
     private static String[] crawlDomains;
 
-    private Map<String, List<String>> images;
-
-    public ImageCrawler() {
-        images = new HashMap<>();
-    }
-
     public static void configure(String[] domain) {
         crawlDomains = domain;
-    }
 
-    public Map<String, List<String>> getImages() {
-        return images;
-    }
-
-    public void setImages(Map<String, List<String>> images) {
-        this.images = images;
     }
 
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
-        String href = url.getURL().toLowerCase();
 
-        if (filters.matcher(href).matches())
+        String href = url.getURL().toLowerCase();
+        if (filters.matcher(href).matches()) {
             return false;
+        }
 
         for (String domain : crawlDomains) {
             if (href.startsWith(domain)) {
                 return true;
             }
         }
-
         return false;
     }
 
-
     @Override
     public void visit(Page page) {
+        int contextLength = 100;
 
+        String patternHTMLTags = "\\<(?!img).*?>";
+        String patternIMG = "\\<img.*?>";
+
+        String patternIMGContext = "(.{1," + Integer.toString(contextLength) + "})"
+                + "(" + patternIMG + ")"
+                + "(.{1," + Integer.toString(contextLength)+ "})";
+
+        String url = page.getWebURL().getURL();
         Document doc = null;
         try {
-            doc = Jsoup.connect(page.getWebURL().getURL()).get();
+            doc = Jsoup.connect(url).get();
+
+            doc.select("script").remove();
+            removeComments(doc);
+
+            String docStr = doc.toString();
+            docStr = docStr.replaceAll(patternHTMLTags, "");
+            docStr = docStr.replaceAll("\\s+", " ");
+
+            Pattern pattern = Pattern.compile(patternIMGContext);
+            Matcher matcher = pattern.matcher(docStr);
+
+            while (matcher.find())
+            {
+                logger.info(matcher.group(2) + " --->  " + matcher.group(1) + " -- " + matcher.group(3));
+            }
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Get all img tags
-        Elements img = doc.getElementsByTag("img");
-
-        int counter = 0;
-
-        // Loop through img tags
-        for (Element el : img) {
-
-            String imageUrl = el.attr("src");
-            String imageContext = el.attr("alt");
-
-            if (images.get(imageUrl) == null)
-                images.put(imageUrl, new ArrayList<>());
-
-            images.get(imageUrl).add(imageContext);
+            logger.warn("Can't connect to url: " + url + ". Skipped!");
         }
     }
 
-
+    private void removeComments(Node node) {
+        for (int i = 0; i < node.childNodes().size();) {
+            Node child = node.childNode(i);
+            if (child.nodeName().equals("#comment"))
+                child.remove();
+            else {
+                removeComments(child);
+                i++;
+            }
+        }
+    }
 }
 
